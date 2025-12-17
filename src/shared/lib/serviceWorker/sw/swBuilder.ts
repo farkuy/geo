97925f49e846dbcbe17810dde93d64635c7d2b97
farkuy <darkuy@mail.ru>
2025-12-17 21:56:57 +0300

    add cache static

--- src/shared/lib/serviceWorker/sw/swBuilder.ts
+++ src/shared/lib/serviceWorker/sw/swBuilder.ts
@@ -1,9 +1,62 @@
 class SwBuilder {
-  _swKey = "builder-v1";
+  _swKey = "geo-cache-v1";
+  _apiKey = "jsonplaceholder.typicode.com";
 
   async addResourcesToCache(resources: RequestInfo[]) {
     const cache = await caches.open(this._swKey);
     await cache.addAll(resources);
+  }
+
+  async putInCache(request: Request, response: Response) {
+    const cache = await caches.open(this._swKey);
+    await cache.put(request, response);
+  }
+
+  async networkError(
+    title?: string,
+    status?: number,
+    headers?: HeadersInit,
+  ): Promise<Response> {
+    return new Response(title ?? "Network error happened 666", {
+      status: status ?? 408,
+      headers: headers ?? { "Content-Type": "text/plain" },
+    });
+  }
+
+  async staticCacheOrNetwork(request: Request, event): Promise<Response> {
+    const responseFromCache = await caches.match(request);
+    if (responseFromCache) {
+      fetch(request).then((res) =>
+        event.waitUntil(this.putInCache(request, res.clone())),
+      );
+      return responseFromCache;
+    }
+
+    try {
+      const responseFromNetwork = await fetch(request);
+      if (responseFromNetwork) {
+        event.waitUntil(this.putInCache(request, responseFromNetwork.clone()));
+        return responseFromNetwork;
+      }
+    } catch (error) {
+      return this.networkError();
+    }
+  }
+
+  async networkWrapper(request: Request, event): Promise<Response> {
+    const path = new URL(request.url);
+    let response: Response;
+    console.log("start wrapper");
+
+    if (path.hostname.includes(this._apiKey)) {
+      console.log("dync");
+      response = await fetch(request);
+    } else {
+      console.log("stat");
+      response = await this.staticCacheOrNetwork(request, event);
+    }
+
+    return response;
   }
 
   async deleteCache(key: string): Promise<void> {
